3dec2ba3ef6d217cc7861de1396ec248272ea609
tinyfind <67093787+tinyfind@users.noreply.github.com>
2022-07-18 09:39:57 +0800

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
  * @LastEditors: dong 2710732812@qq.com
  * @LastEditTime: 2022-05-16 10:15:28
  * @FilePath: \hot_reload\server.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 const express = require('express')
 const fs = require('fs')
